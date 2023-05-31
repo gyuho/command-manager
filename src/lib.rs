4f6d27d688f4738b9fc423f0ea3316f6a0089a54
Gyuho Lee <gyuho.lee@avalabs.org>
2023-05-31 00:44:04 -0700

    0.0.2
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,35 @@
 use std::{
     io::{self, Error, ErrorKind},
     process::Command,
-    string::String,
 };
 
-use log::{info, warn};
+#[derive(Debug, Clone)]
+pub struct Output {
+    pub stdout: String,
+    pub stderr: String,
+}
 
-/// Runs bash command(s) and returns the result.
+/// Runs shell command(s) and returns the output.
 #[allow(dead_code)]
-pub fn run(cmd: &str) -> io::Result<(String, String)> {
-    info!("running {}", cmd);
+pub fn run(cmd: &str) -> io::Result<Output> {
+    log::debug!("running '{cmd}'");
     match Command::new("sh").args(&["-c", cmd]).output() {
         Ok(o) => {
             let stdout = String::from_utf8_lossy(&o.stdout).as_ref().to_owned();
             let stderr = String::from_utf8_lossy(&o.stderr).as_ref().to_owned();
             if o.status.success() {
-                info!("command run success");
-                Ok((stdout, stderr))
+                Ok(Output { stdout, stderr })
             } else {
                 match o.status.code() {
                     Some(code) => {
-                        warn!("command failed with status code {}: {}", code, stderr);
+                        log::warn!("command failed with status code {}: {}", code, stderr);
                         Err(Error::new(
                             ErrorKind::Other,
                             format!("command failed with status code {}: {}", code, stderr),
                         ))
                     }
                     None => {
-                        warn!(
+                        log::warn!(
                             "command terminated by signal with no status code: {}",
                             stderr
                         );
@@ -43,7 +45,7 @@ pub fn run(cmd: &str) -> io::Result<(String, String)> {
             }
         }
         Err(e) => {
-            warn!("command failed: {}", e);
+            log::warn!("command failed: {}", e);
             Err(e)
         }
     }
@@ -51,9 +53,7 @@ pub fn run(cmd: &str) -> io::Result<(String, String)> {
 
 #[test]
 fn test_bash_run() {
-    let ret = run("ls -lah .");
-    assert!(ret.is_ok());
-    let t = ret.unwrap();
-    println!("stdout:\n{}", t.0);
-    println!("stderr:\n{}", t.1);
+    let output = run("ls -lah .").unwrap();
+    println!("stdout:\n{}", output.stdout);
+    println!("stderr:\n{}", output.stderr);
 }
